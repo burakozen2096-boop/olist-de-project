de02018e1723b73939366554368ec6ac43f7f159
Burak Ozen <burakozen2096@gmail.com>
2026-01-11 19:04:12 +0300

    docs: register raw sources and add placeholder models

---
+++ dbt/olist_dbt/models/marts/_placeholder_marts.sql
@@ -0,0 +1,3 @@
+-- placeholder model to activate dbt_project.yml config paths
+-- Will be replaced by real mart models (dims/facts) after staging is implemented.
+select 1 as placeholder
---
+++ dbt/olist_dbt/models/staging/_placeholder_staging.sql
@@ -0,0 +1,3 @@
+-- placeholder model to activate dbt_project.yml config paths
+-- Will be replaced by real staging models (e.g., stg_orders, stg_customers).
+select 1 as placeholder
